dba862d923bc902b2b520074f6b4de42b0eb12a5
Oleg <hengelwm@mail.ru>
2020-09-04 22:55:49 +0300

    Film Commit1

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
 'use strict';
 
-// const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-// const personalMovieDB = {
-//     count: numberOfFilms,
-//     movies: {},
-//     actors: {},
-//     genres: [],
-//     privat: false
-// };
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
 
-// const a = prompt('Один из последних просмотренных фильмов?', ''),
-//       b = prompt('На сколько оцените его?', ''),
-//       c = prompt('Один из последних просмотренных фильмов?', ''),
-//       d = prompt('На сколько оцените его?', '');
+for (let i = 0; i < 2; i++  ){
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+          b = prompt('На сколько оцените его?', '');
 
-// personalMovieDB.movies[a] = b;
-// personalMovieDB.movies[c] = d;
+          personalMovieDB.movies[a] = b;
+          
+}
 
-// console.log(personalMovieDB);
+console.log(personalMovieDB);
 
-let num = 48;
+// let num = 48;
 
 // while (num <= 55) {
 //     console.log(num);
